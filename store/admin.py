b559e27d014b424969531b4102790a8d27152bac
Ayvaz Izmailov <saltinthecut@gmail.com>
2023-09-06 19:47:40 +0300

    The Book model, serializer and ViewSet are created. Registered path

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Book
+
+
+@admin.register(Book)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    fields = ("name", "price")
+    search_fields = ("name",)
+    ordering = ("name",)
